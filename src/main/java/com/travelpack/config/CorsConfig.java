d0825f55eb3679394264f15a659f3fc0971acce7
shubham <shub23c@gmail.com>
2026-04-22 13:49:21 +0530

    cors

--- src/main/java/com/travelpack/config/CorsConfig.java
+++ src/main/java/com/travelpack/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://travelpack-in.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
